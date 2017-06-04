a3d3029290f9da020d81ea73fb15f4191ba6796c
yukirin <standupdown@gmail.com>
2017-06-05 00:28:48 +0900

    Add Pos Data

---
+++ data/plot.py
@@ -0,0 +1,14 @@
+import csv
+import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+
+with open("posdata.csv", 'r') as f:
+    r = csv.reader(f)
+    d = np.array(list(r), dtype=np.float32)
+    x, y, z = d[:, 0], d[:, 1], d[:, 2]
+
+fig = plt.figure()
+ax = Axes3D(fig)
+p = ax.scatter3D(x, y, z)
+plt.show()
